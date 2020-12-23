93c147fda5a01b759ef2b9dda55b5e7b53be1d74
Jonny Le <jonnyle2@gmail.com>
2020-12-22 22:59:01 -0600

    gdrive link

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home(){
         <h1>Jonny M. Le</h1>
         <p>
           Recent graduate looking for a full-time position in development in the DFW and neighboring areas. 
-          Here is his <a href='./jonnys-resume.pdf'>resume</a>! It's not much, but its a start and he wants to find a company to help him build it.
+          Here is his <a href='https://drive.google.com/file/d/1z9Ayd0OohkplAMJbNUaJHIMaWWW-20Tv/view?usp=sharing'>resume</a>! It's not much, but its a start. He would love to work with a company to help him build it.
         </p>
       </header>
     </div>
