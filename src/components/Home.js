97220f160418e7e264da220b73bd5c2c534aaea6
Jonny <jonnyle2@gmail.com>
2020-12-26 06:01:53 -0600

    added projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import './Home.css'
+import Project from './Project.js';
+import CheckInDemo from './resources/checklist-demo.gif';
 
 function Home(){
   return (
@@ -10,6 +12,20 @@ function Home(){
           Here is his <a href='https://drive.google.com/file/d/1z9Ayd0OohkplAMJbNUaJHIMaWWW-20Tv/view?usp=sharing'>resume</a>! It's not much, but its a start. He would love to work with a company to help him build it.
         </p>
       </header>
+
+      {/* Project Website */}
+      <Project className='project-odd'
+               title='This Website'
+               summary='It is a simple website to have more practice with react, build and test my chatbot, and showcase my projects.'
+               techList={['React', 'Javascript', 'HTML', 'CSS']}
+               imageSrc='' imageAlt='GIF of website'/>
+
+      {/* Project Checklist */}
+      <Project className='project-even'
+               title='Check-In'
+               summary='Intuitive check-in web application that solves issues of paper checklist. It features a dynamic list view and authority limited access through authentication.'
+               techList={['SpringBoot', 'Java', 'MySQL', 'HTML', 'CSS']}
+               imageSrc={CheckInDemo} imageAlt='GIF of check-in demo'/>
     </div>
   );
 }
