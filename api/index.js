53b0faed552e3b2b4c9c795ae3c8af6bd312956a
Jonny Le <jonnyle2@gmail.com>
2021-01-05 02:22:08 -0600

    initial commit

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Contact = require('./models/contacts');
+
+const MONGODB_URL = '...'
+
+mongoose.connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(_ => console.log("DB connected"))
+.catch(error => console.log(error));
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// listen POST new contact
+app.post('/api/contact', (req, res) => {
+    const contact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.message
+    }).save((err, response) => {
+        if(err)
+            return res.status(400).send(err);
+        res.status(200).send(response);
+    });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port);
+console.log(`Server listening on ${port}`); 
